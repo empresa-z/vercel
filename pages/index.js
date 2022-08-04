dcc49edd36eba758ec150f389ed9bf3dd381c972
Andre Renault <andre@zimboli.com>
2022-08-03 23:35:34 -0300

    fix for > htmlFor

--- pages/index.js
+++ pages/index.js
@@ -358,7 +358,7 @@ export default function Home() {
               <form className="p-2 pb-0 w-full">
                 <div className="mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
                   <div className="py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800">
-                    <label for="comment" className="sr-only">Your comment</label>
+                    <label htmlFor="comment" className="sr-only">Your comment</label>
                     <textarea id="comment" rows="4" className="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." required=""></textarea>
                   </div>
                   <div className="flex justify-between items-center py-2 px-3 border-t dark:border-gray-600">
